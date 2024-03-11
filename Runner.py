acc892e8821ca54579ac1e675318ea35a7e8ede1
Ramesh <114616616+capnmav77@users.noreply.github.com>
2024-03-11 13:35:51 +0530

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -5,7 +5,7 @@ from .DataGen import process_and_store_data
 
 def synthesise_data(folder_name,update):
 		print("Started The Downloadin")
-		download_files_from_blob_storage("DefaultEndpointsProtocol=https;AccountName=llmaodatastore;AccountKey=J+go6cxXfi7v4Exq2ggINtxr13JICvHBoPjuKmGN5R0Ly6palZsO1RYiSOjvigxwx3jc1rOVPCI8+AStU/dpYQ==;EndpointSuffix=core.windows.net"
+		download_files_from_blob_storage("BLOB_STORAGE_LINK"
                                  ,folder_name, 
                                  "/home/llmao/fastapi/DataGen/corpus", 
                                  "data-corpus")
